f2c8f2ebba300a45daca2af09c6e63628910cb58
Ojuntunen <t1juol00@students.oamk.fi>
2022-11-29 17:32:58 +0200

    working tcp socket client

--- python/tcp-socket.py
+++ python/tcp-socket.py
@@ -6,6 +6,19 @@ PORT = 20000
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b"70\n")
-    data = s.recv(1024)
+    connected = True
+    data = []
+    while 1:
+        msg = s.recv(1024)
+        if msg:
+            data.append(msg)
+        else:
+            break
+        # data = s.recv(1024)
+        # data = []
+        
+    print(f"received {data}")
     
-print(f"received {data}")
+with open("./python/TCT_socket_data.csv", "w") as file:
+    for i in range(len(data)):
+        file.write(data[i].decode("utf-8"))
